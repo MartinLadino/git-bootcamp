046286582483c4b9774a9862b1d71a2cb83d3429
Martin Ladino <jmartin.ladino.moreno@gmail.com>
2022-05-19 18:20:49 -0500

    use classes

--- katas.py
+++ katas.py
@@ -1,3 +1,12 @@
+class Unities:
+    def __init__(self,name,age,email,password):
+        self.Name = name
+        self.Age = age
+        self.Email = email
+        self.Password = password
+
+
+
 name = 'Martin'
 age = 28
 email = 'jmartin.ladino@gmail.com'
