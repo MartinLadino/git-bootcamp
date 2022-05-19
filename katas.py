dee79cf1a44db332dc343bddcbf0ea4ebb66b48a
Martin Ladino <jmartin.ladino.moreno@gmail.com>
2022-05-19 15:51:04 -0500

    add grades info

--- katas.py
+++ katas.py
@@ -1,4 +1,10 @@
 name = 'Martin'
 age = 28
 email = 'jmartin.ladino@gmail.com'
+grades = {
+    "maths":10,
+    "science":8,
+    "spanish":9,
+    "arts":7
+}
 print(f"your name is{name} you're {28} years old and extra info: {email}")
