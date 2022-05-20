fa6d442197f799c3924db922dacf136a84d529ef
Martin Ladino <jmartin.ladino.moreno@gmail.com>
2022-05-20 18:50:25 -0500

    classes

--- katas.py
+++ katas.py
@@ -1,19 +1,17 @@
+from pip import main
+
+
 class Unities:
     def __init__(self,name,age,email,password):
-        self.Name = name
-        self.Age = age
+        self.name = name
+        self.age = age
         self.Email = email
         self.Password = password
+    
+    def info(self):
+        print(self.name)
 
+if __name__ == "__main__":
 
-
-name = 'Martin'
-age = 28
-email = 'jmartin.ladino@gmail.com'
-grades = {
-    "maths":10,
-    "science":8,
-    "spanish":9,
-    "arts":7
-}
-print(f"your name is{name} you're {28} years old and extra info: {email}")
+    person = Unities("martin", 28, "martin@gmail.com", 11223322)
+    person.info()
