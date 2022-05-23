3245d222a2d3305b9419a93113df5215d33c63ca
Martin Ladino <jmartin.ladino.moreno@gmail.com>
2022-05-22 22:42:51 -0500

    Working Version

--- ReplaceLast.py
+++ ReplaceLast.py
@@ -1,8 +1,11 @@
 def replace_last(line: list) -> list:
     first = 0
-    line.insert(first,line[len(line)-1])
-    line.pop(len(line)-1)
-    return line
+    if len(line) > 1:
+        line.insert(first,line[len(line)-1])
+        line.pop(len(line)-1)
+        return line
+    else:
+        return line
 
 if __name__=='__main__':
     print(replace_last([2,3,4,1]))
