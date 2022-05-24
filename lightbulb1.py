ce2e5650606b98092e4cde3f81aa60e2de894f2a
Martin Ladino <jmartin.ladino.moreno@gmail.com>
2022-05-23 21:50:00 -0500

    oreily4f

--- lightbulb1.py
+++ lightbulb1.py
@@ -4,16 +4,18 @@ from typing import List
 
 def sum_light(els: List[datetime]) -> int:
     lenght = len(els) - 1
-
+    totaltime = 0
     while lenght > 0:
         totaltime += abs(timedelta.total_seconds(els[lenght] - els[lenght - 1]))
         lenght -= 2
+    
+    return int(totaltime)
 
     
 if __name__=='__main__':
     print(sum_light([
         datetime(2015, 1, 12, 10, 0 , 0),
-        datetime(2015, 1, 12, 10, 10 , 10),
+        datetime(2015, 1, 12, 10, 0 , 10),
         datetime(2015, 1, 12, 11, 0 , 0),
-        datetime(2015, 1, 12, 11, 10 , 10),
+        datetime(2015, 1, 13, 11, 0 , 0),
     ]))
