c9502eb7ccf732b3e6c2205b1742f9b15b759528
Martin Ladino <jmartin.ladino.moreno@gmail.com>
2022-05-24 21:25:52 -0500

    tictactoe

--- YicTacToe.py
+++ YicTacToe.py
@@ -4,23 +4,18 @@ from typing import List
 def checkio(game_result: List[str]) -> str:
     Owin = False
     Xwin = False
-    for x in game_result:
-        if x.count('X') > 2:
-            Xwin = True
-        elif x.count('O') > 2:
-            Owin = True
+    vertical = [game_result[0][0] + game_result[1][0] + game_result[2][0],
+                game_result[0][1] + game_result[1][1] + game_result[2][1],
+                game_result[0][2] + game_result[1][2] + game_result[2][2]]
     
-    if Xwin and Owin:
-        return 'D'
-    elif Xwin:
-        return 'X'
-    elif Owin:
-        return 'O'
+    Diagonal = [game_result[0][0] + game_result[1][1] + game_result[2][2],
+                game_result[0][2] + game_result[1][1] + game_result[2][0]]
+        
 
 
 
 if __name__=='__main__':
     print(checkio([
-    "XXX",
+    "XOX",
     "XX.",
-    "OOO"]))
+    "XOO"]))
